4c89a60469e9e3608f84b3062ba281b45888b552
Artem Korotkov <ars.korotkov@gmail.com>
2020-04-14 01:55:00 +0300

    Fix compile error due to fmt (async_trait macro)

--- src/api/v2/resource.rs
+++ src/api/v2/resource.rs
@@ -15,34 +15,28 @@ pub trait ApiNamedResource: ApiResource {
     fn name(&self) -> String;
 }
 
+#[rustfmt::skip]
 #[async_trait]
 pub trait ApiResourceList {
-    type ResourceType: Sized;
+    type ResourceType : Sized;
 
     fn count(&self) -> usize;
 
-    async fn next_list(
-        &self,
-    ) -> Result<Option<Box<dyn ApiResourceList<ResourceType = Self::ResourceType>>>>;
-    async fn previous_list(
-        &self,
-    ) -> Result<Option<Box<dyn ApiResourceList<ResourceType = Self::ResourceType>>>>;
+    async fn next_list(&self) -> Result<Option<Box<dyn ApiResourceList<ResourceType = Self::ResourceType>>>>;
+    async fn previous_list(&self) -> Result<Option<Box<dyn ApiResourceList<ResourceType = Self::ResourceType>>>>;
 
     fn resources(&self) -> Vec<Box<dyn ApiResource<ResourceType = Self::ResourceType>>>;
 }
 
+#[rustfmt::skip]
 #[async_trait]
 pub trait ApiNamedResourceList {
-    type ResourceType: Sized;
+    type ResourceType : Sized;
 
     fn count(&self) -> usize;
 
-    async fn next_list(
-        &self,
-    ) -> Result<Option<Box<dyn ApiNamedResourceList<ResourceType = Self::ResourceType>>>>;
-    async fn previous_list(
-        &self,
-    ) -> Result<Option<Box<dyn ApiNamedResourceList<ResourceType = Self::ResourceType>>>>;
+    async fn next_list(&self) -> Result<Option<Box<dyn ApiNamedResourceList<ResourceType = Self::ResourceType>>>>;
+    async fn previous_list(&self) -> Result<Option<Box<dyn ApiNamedResourceList<ResourceType = Self::ResourceType>>>>;
 
     fn resources(&self) -> Vec<Box<dyn ApiNamedResource<ResourceType = Self::ResourceType>>>;
 }
@@ -127,17 +121,18 @@ macro_rules! decl_named_resource {
     };
 }
 
+#[rustfmt::skip]
 macro_rules! decl_resource_list {
     ($list:tt for $model:tt with $res:tt) => {
         pub struct $list {
             client: $crate::api::v2::client::ApiClient,
             resource_list: $crate::models::v2::resource::ResourceList,
         }
-
+        
         impl $list {
             pub(crate) fn new(
                 client: $crate::api::v2::client::ApiClient,
-                resource_list: $crate::models::v2::resource::ResourceList,
+                resource_list: $crate::models::v2::resource::ResourceList
             ) -> Self {
                 Self {
                     client,
@@ -150,82 +145,66 @@ macro_rules! decl_resource_list {
         impl $crate::api::v2::resource::ApiResourceList for $list {
             type ResourceType = $model;
 
-            fn count(&self) -> usize {
-                self.resource_list.count
-            }
+            fn count(&self) -> usize { self.resource_list.count }
 
-            async fn next_list(
-                &self,
-            ) -> Result<
-                Option<
-                    Box<
-                        dyn $crate::api::v2::resource::ApiResourceList<
-                            ResourceType = Self::ResourceType,
-                        >,
-                    >,
-                >,
-            > {
+            async fn next_list(&self) ->
+            Result<Option<Box<dyn $crate::api::v2::resource::ApiResourceList<ResourceType = Self::ResourceType>>>> {
                 match self.resource_list.next {
                     None => Ok(None),
-                    Some(ref url) => Ok(Some(Box::new(Self::new(
-                        self.client.clone(),
-                        self.client
-                            .request(url)
-                            .await?
-                            .json::<$crate::models::v2::resource::ResourceList>()
-                            .await?,
-                    )))),
+                    Some(ref url) => {
+                        Ok(Some(Box::new(Self::new(
+                            self.client.clone(),
+                            self.client
+                                .request(url)
+                                .await?
+                                .json::<$crate::models::v2::resource::ResourceList>()
+                                .await?
+                        ))))
+                    }
                 }
             }
 
-            async fn previous_list(
-                &self,
-            ) -> Result<
-                Option<
-                    Box<
-                        dyn $crate::api::v2::resource::ApiResourceList<
-                            ResourceType = Self::ResourceType,
-                        >,
-                    >,
-                >,
-            > {
+            async fn previous_list(&self) ->
+            Result<Option<Box<dyn $crate::api::v2::resource::ApiResourceList<ResourceType = Self::ResourceType>>>> {
                 match self.resource_list.previous {
                     None => Ok(None),
-                    Some(ref url) => Ok(Some(Box::new(Self::new(
-                        self.client.clone(),
-                        self.client
-                            .request(url)
-                            .await?
-                            .json::<$crate::models::v2::resource::ResourceList>()
-                            .await?,
-                    )))),
+                    Some(ref url) => {
+                        Ok(Some(Box::new(Self::new(
+                            self.client.clone(),
+                            self.client
+                                .request(url)
+                                .await?
+                                .json::<$crate::models::v2::resource::ResourceList>()
+                                .await?
+                        ))))
+                    }
                 }
             }
-
+            
             fn resources(&self) -> Vec<Box<dyn ApiResource<ResourceType = Self::ResourceType>>> {
-                let mut resources =
-                    Vec::<Box<dyn ApiResource<ResourceType = Self::ResourceType>>>::new();
+                let mut resources = Vec::<Box<dyn ApiResource<ResourceType = Self::ResourceType>>>::new();
                 for res in self.resource_list.results.iter() {
-                    resources.push(Box::new($res::new(self.client.clone(), res.clone())));
+                    resources.push(Box::new($res::new(self.client.clone(),res.clone())));
                 }
 
                 resources
-            }
+            }    
         }
     };
 }
 
+#[rustfmt::skip]
 macro_rules! decl_named_resource_list {
     ($list:tt for $model:tt with $res:tt) => {
         pub struct $list {
             client: $crate::api::v2::client::ApiClient,
             resource_list: $crate::models::v2::resource::NamedResourceList,
         }
-
+        
         impl $list {
             pub(crate) fn new(
                 client: $crate::api::v2::client::ApiClient,
-                resource_list: $crate::models::v2::resource::NamedResourceList,
+                resource_list: $crate::models::v2::resource::NamedResourceList
             ) -> Self {
                 Self {
                     client,
@@ -238,69 +217,50 @@ macro_rules! decl_named_resource_list {
         impl $crate::api::v2::resource::ApiNamedResourceList for $list {
             type ResourceType = $model;
 
-            fn count(&self) -> usize {
-                self.resource_list.count
-            }
+            fn count(&self) -> usize { self.resource_list.count }
 
-            async fn next_list(
-                &self,
-            ) -> Result<
-                Option<
-                    Box<
-                        dyn $crate::api::v2::resource::ApiNamedResourceList<
-                            ResourceType = Self::ResourceType,
-                        >,
-                    >,
-                >,
-            > {
+            async fn next_list(&self) ->
+            Result<Option<Box<dyn $crate::api::v2::resource::ApiNamedResourceList<ResourceType = Self::ResourceType>>>> {
                 match self.resource_list.next {
                     None => Ok(None),
-                    Some(ref url) => Ok(Some(Box::new(Self::new(
-                        self.client.clone(),
-                        self.client
-                            .request(url)
-                            .await?
-                            .json::<$crate::models::v2::resource::NamedResourceList>()
-                            .await?,
-                    )))),
+                    Some(ref url) => {
+                        Ok(Some(Box::new(Self::new(
+                            self.client.clone(),
+                            self.client
+                                .request(url)
+                                .await?
+                                .json::<$crate::models::v2::resource::NamedResourceList>()
+                                .await?
+                        ))))
+                    }
                 }
             }
 
-            async fn previous_list(
-                &self,
-            ) -> Result<
-                Option<
-                    Box<
-                        dyn $crate::api::v2::resource::ApiNamedResourceList<
-                            ResourceType = Self::ResourceType,
-                        >,
-                    >,
-                >,
-            > {
+            async fn previous_list(&self) ->
+            Result<Option<Box<dyn $crate::api::v2::resource::ApiNamedResourceList<ResourceType = Self::ResourceType>>>> {
                 match self.resource_list.previous {
                     None => Ok(None),
-                    Some(ref url) => Ok(Some(Box::new(Self::new(
-                        self.client.clone(),
-                        self.client
-                            .request(url)
-                            .await?
-                            .json::<$crate::models::v2::resource::NamedResourceList>()
-                            .await?,
-                    )))),
+                    Some(ref url) => {
+                        Ok(Some(Box::new(Self::new(
+                            self.client.clone(),
+                            self.client
+                                .request(url)
+                                .await?
+                                .json::<$crate::models::v2::resource::NamedResourceList>()
+                                .await?
+                        ))))
+                    }
                 }
             }
-
-            fn resources(
-                &self,
-            ) -> Vec<Box<dyn ApiNamedResource<ResourceType = Self::ResourceType>>> {
-                let mut resources =
-                    Vec::<Box<dyn ApiNamedResource<ResourceType = Self::ResourceType>>>::new();
+            
+            fn resources(&self) -> Vec<Box<dyn ApiNamedResource<ResourceType = Self::ResourceType>>> {
+                let mut resources = Vec::<Box<dyn ApiNamedResource<ResourceType = Self::ResourceType>>>::new();
                 for res in self.resource_list.results.iter() {
-                    resources.push(Box::new($res::new(self.client.clone(), res.clone())));
+                    resources.push(Box::new($res::new(self.client.clone(),res.clone())));
                 }
 
                 resources
-            }
+            }    
         }
     };
 }
